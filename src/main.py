67cff607aa282fe5911e7c1fb070bf529e40cbf8
lhfu <lhfu@dataman-inc.com>
2016-11-28 11:12:31 +0800

    Init

---
+++ src/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'f0x11'
+
+
+class A(object):
+    pass
+
+
+class T(object):
+    pass
+
+
+class C(object):
+    pass
+
+
+class G(object):
+    pass
+
+
+class U(object):
+    pass
